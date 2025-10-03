c8959b1ad7d43cf52dd8f3df91de5f1f16864573
marcwalkr <47399441+marcwalkr@users.noreply.github.com>
2025-10-03 12:19:06 -0500

    Add Display module

--- script.js
+++ script.js
@@ -39,3 +39,36 @@ const Board = (function () {
 
   return { getCell, placeMarker, clear, filled }
 })();
+
+const Display = (function () {
+  const promptPlayerData = () => {
+    const player1Name = prompt("Enter name for Player One");
+    const player1Marker = prompt("Enter marker for Player One");
+
+    const player2Name = prompt("Enter name for Player Two");
+    const player2Marker = prompt("Enter marker for Player Two");
+
+    const player1 = createPlayer(player1Name, player1Marker);
+    const player2 = createPlayer(player2Name, player2Marker);
+
+    return [player1, player2];
+  }
+
+  const promptMove = (playerName) => {
+    const answer = prompt(`${playerName}'s Turn\nEnter a position to place your marker.`);
+    let [row, column] = answer.split(" ");
+
+    row = Number(row);
+    column = Number(column);
+
+    return { row, column }
+  }
+
+  const print = () => {
+    console.log(`${Board.getCell(0, 0)}\t${Board.getCell(0, 1)}\t${Board.getCell(0, 2)}`);
+    console.log(`${Board.getCell(1, 0)}\t${Board.getCell(1, 1)}\t${Board.getCell(1, 2)}`);
+    console.log(`${Board.getCell(2, 0)}\t${Board.getCell(2, 1)}\t${Board.getCell(2, 2)}`);
+  }
+
+  return { promptPlayerData, promptMove, print };
+})(Board);
