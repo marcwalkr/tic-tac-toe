e68b906bbb25d0f3e88b24b90fedbecbd6a8fb5b
marcwalkr <47399441+marcwalkr@users.noreply.github.com>
2025-10-03 12:18:17 -0500

    Add Board module

--- script.js
+++ script.js
@@ -5,3 +5,37 @@ function createPlayer(name, marker) {
 
   return { name, marker, getWins, addWin };
 }
+
+const Board = (function () {
+  let board = [
+    ["", "", ""],
+    ["", "", ""],
+    ["", "", ""]
+  ];
+
+  const getCell = (row, column) => board[row][column];
+
+  const placeMarker = (marker, row, column) => {
+    board[row][column] = marker;
+  }
+
+  const clear = () => {
+    board = [
+      ["", "", ""], 
+      ["", "", ""], 
+      ["", "", ""]
+    ];
+  }
+
+  const filled = () => {
+    for (const row of board) {
+      for (const cell of row) {
+        if (cell === "") return false;
+      }
+    }
+
+    return true;
+  }
+
+  return { getCell, placeMarker, clear, filled }
+})();
